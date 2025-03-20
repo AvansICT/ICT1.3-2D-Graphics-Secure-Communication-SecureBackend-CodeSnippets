0894724d4b666d05d584bef8e6b418ddc904aee7
Marc Mathijssen <maa.mathijssen@avans.nl>
2025-03-20 15:20:03 +0100

    SQL db owning user

--- 04_remotegit_cloud/create_database_user.sql
+++ 04_remotegit_cloud/create_database_user.sql
@@ -2,5 +2,6 @@ CREATE USER MY_APPLICATION_USER WITH PASSWORD = '(a safe password here)'
 
 EXEC sp_addrolemember 'db_datawriter', 'MY_APPLICATION_USER';
 EXEC sp_addrolemember 'db_datareader', 'MY_APPLICATION_USER';
-
+-- Uncomment this line if you would like the user to have full database administrator rights
+-- EXEC sp_addrolemember 'db_owner', 'MY_APPLICATION_USER' 
 
