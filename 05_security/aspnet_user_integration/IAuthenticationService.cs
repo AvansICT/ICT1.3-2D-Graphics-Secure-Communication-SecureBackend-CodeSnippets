fce077f40f32263ede1cb89f7da2a0d2d2b87cd9
avans-marc <maa.mathijssen@avans.nl>
2026-02-13 16:40:53 +0100

    (re)moved some files

--- 05_security/aspnet_user_integration/IAuthenticationService.cs
+++
@@ -1,8 +0,0 @@
-    public interface IAuthenticationService
-    {
-        /// <summary>
-        /// Returns the user name of the authenticated user
-        /// </summary>
-        /// <returns></returns>
-        string? GetCurrentAuthenticatedUserId();
-    }
--- 05_security/aspnet_user_integration/Program.cs
+++
@@ -1,4 +0,0 @@
-// Adding the HTTP Context accessor to be injected. This is needed by the AspNetIdentityUserRepository
-// to resolve the current user.
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddTransient<IAuthenticationService, AspNetIdentityAuthenticationService>();
